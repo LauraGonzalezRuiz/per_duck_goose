6cf83ba8ca42821f97aa36ab73b5aaa93c46bf3d
Laura <l.gonzalezru.2018@alumnos.urjc.es>
2019-01-21 16:15:10 +0100

    mi commit

---
+++ pato.py
@@ -0,0 +1,9 @@
+lista = [a, b, c, d, e, f, g, h]
+def patopato(lista, posicion):
+    while posicion > len(lista):
+      posicion = posicion - len(lista)
+    posicion = posicion + 1
+    print(lista[posicion])
+    return 
+input
+
